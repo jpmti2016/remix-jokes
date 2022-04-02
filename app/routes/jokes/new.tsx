3d5080e4ac81b00c19cc4c9bd1042ac6ace01c5c
Yampier Medina <jpmti2016@gmail.com>
2022-04-01 21:48:49 -0400

    Create routes

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,22 @@
+export default function NewJokeRoute() {
+    return (
+        <div>
+            <p>Add your own hilarious joke</p>
+            <form method="post">
+                <div>
+                    <label>
+                        Name: <input type="text" name="name" />
+                    </label>
+
+                    <label >
+                        Content: <textarea name="content" />
+                    </label>
+
+                    <div>
+                        <button type="submit" className="button">Add</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
